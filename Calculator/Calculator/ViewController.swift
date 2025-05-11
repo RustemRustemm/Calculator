7271f1d27a6b0692e853daef27df9b23cc86483f
Rustam Shakirzianov <rustam4m@Mac-mini-Rustam.local>
2025-05-11 18:13:40 +0300

    Configured to Storyboard Free

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rustam Shakirzianov on 11.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
